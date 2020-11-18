248b6094a9aa14a0272d8a35cb233293c118feb5
HaotianTeng <havens.teng@gmail.com>
2020-11-18 10:17:53 -0500

    Fix the sample method in Memory class; Make train_step function in the global scope ;Add frame in to the memory when game start

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -27,16 +27,16 @@ class Enviroment(object):
     def __init__(self,config):
         self.env = gym.make(config.ENV_NAME)
         self.random_start = config.RANDOM_START
-    
+        self.config = config
     def new_game(self):
         frame = self.env.reset()
         for _ in np.arange(random.randint(0,self.random_start)):
             frame, reward, done, info = self.env.step(0)
-        return preprocess(frame)
+        return preprocess(frame,self.config.IMG_DTYPE)
     
     def step(self,action):
         frame, reward, done, info = self.env.step(action)
-        return preprocess(frame),reward,done,info
+        return preprocess(frame,self.config.IMG_DTYPE),reward,done,info
     
     def random_step(self):
         action = self.env.action_space.sample()
@@ -46,16 +46,17 @@ class Enviroment(object):
 class ReplayMemory(object):
     def __init__(self, config):
         self.capacity = config.BUFFER_SIZE
+        self.batch_size = config.BATCH_SIZE
         self.position = 0
         self.count = 0
         self.history_length = config.FRAME_STACK
         #Preallocate Memory to ensure the RAM has enough capacity
-        self.images = np.empty((self.capacity,config.IMAGE_SIZE[0],config.IMAGE_SIZE[1]),dtype = np.float16)
+        self.images = np.empty((self.capacity,config.IMAGE_SIZE[0],config.IMAGE_SIZE[1]),dtype = config.IMG_DTYPE)
         self.actions = np.empty(self.capacity,dtype = np.uint8)
         self.rewards = np.empty(self.capacity,dtype = np.int)
         self.done = np.empty(self.capacity,dtype = np.bool)
-        self.state = np.empty((config.BATCH_SIZE,config.FRAME_STACK)+config.IMAGE_SIZE,dtype = np.float32)
-        self.next_state = np.empty((config.BATCH_SIZE,config.FRAME_STACK)+config.IMAGE_SIZE,dtype = np.float32)
+        self.state = np.empty((self.batch_size,config.FRAME_STACK)+config.IMAGE_SIZE,dtype = np.float32)
+        self.next_state = np.empty((self.batch_size,config.FRAME_STACK)+config.IMAGE_SIZE,dtype = np.float32)
         
     def push(self, image, action, reward, done):
         """Saves a transition."""
@@ -70,10 +71,10 @@ class ReplayMemory(object):
         assert self.count > 0, "replay memory is empy, use at least --random_steps 1"
         return self.images[(index - (self.history_length - 1)):(index + 1), ...]
 
-    def sample(self, batch_size):
+    def sample(self):
         assert self.count > self.history_length
         indexes = []
-        while len(indexes) < batch_size:
+        while len(indexes) < self.batch_size:
             while True:
                 index = random.randint(self.history_length, self.count - 1)
                 # if wraps over current pointer, then get new one
@@ -86,7 +87,7 @@ class ReplayMemory(object):
                 break
           
             # NB! having index first is fastest in C-order matrices
-            self.state[len(indexes), ...] = self.get_state(index - 1)
+            self.state[len(indexes), ...] = self.get_state(index-1)
             self.next_state[len(indexes), ...] = self.get_state(index)
             indexes.append(index)
     
@@ -109,22 +110,22 @@ class DQN(nn.Module):
 
     def __init__(self, config): 
         super(DQN, self).__init__()
-
+        self.device = config.DEVICE
         # an affine operation: y = Wx + b
         self.conv1 = nn.Conv2d(
             in_channels=config.FRAME_STACK,
-            out_channels=16,
+            out_channels=32,
             kernel_size=8,
             stride=4,
-            padding=0)
+            padding=2)
         out_shape = self.get_shape(np.array(config.IMAGE_SIZE),
                               padding = np.array(self.conv1.padding),
                               kernal_size = np.array(self.conv1.kernel_size),
                               stride = np.array(self.conv1.stride),
                               dilation = np.array(self.conv1.dilation))
         self.conv2 = nn.Conv2d(
-            in_channels=16,
-            out_channels=32,
+            in_channels=32,
+            out_channels=64,
             kernel_size=4,
             stride=2,
             padding=1)
@@ -149,16 +150,12 @@ class DQN(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.out(x)
         return x
-    def predict(self,state,eps):
-        q_vals = self(state.to(device).unsqueeze(0)).squeeze()
-        if np.random.rand() < eps:
-            return np.random.randint(q_vals.shape[0])
-        return q_vals.argmax().item()
+
 
 class History(object):
     def __init__(self,config):
         self.stack_n = config.FRAME_STACK
-        self.buffer = np.zeros((self.stack_n,config.IMAGE_SIZE[0],config.IMAGE_SIZE[1]),dtype = np.float16)
+        self.buffer = np.zeros((self.stack_n,config.IMAGE_SIZE[0],config.IMAGE_SIZE[1]),dtype = config.IMG_DTYPE)
     
     def add(self,image):
         self.buffer[:-1] = self.buffer[1:]
@@ -168,77 +165,28 @@ class History(object):
         return torch.from_numpy(self.buffer.astype(np.float32))
 
 
-def init_memory(env,buffer,initial_size):
+def init_memory(env,buffer,initial_size,history_length):
     frame = env.new_game()
+    for _ in np.arange(history_length):
+        buffer.push(frame,0,0,False)
     for _ in tqdm(np.arange(initial_size)):
         frame,action,reward,done,info = env.random_step()
         buffer.push(frame,action,reward,done)
         if done:
             env.new_game()
-
-def simulate(env, horizon, policy,config, render = False):
-    tot_reward = 0
-    frame = env.new_game()
-    frame_buffer = History(config)
-    for _ in np.arange(config.FRAME_STACK):
-        frame_buffer.add(frame)
-    movie_frame = []
-    for t in range(horizon):
-        if render:
-            #env.render()
-            env.env.render()
-            movie_frame.append(env.env.render(mode="rgb_array"))
-            time.sleep(1/24)
-            
-        state = frame_buffer.get()
-        action = policy.predict(state,eps = 0)
-        frame, reward, done, info = env.step(action)
-        frame_buffer.add(frame)
-        tot_reward += reward
-        if done:
-            break
-            
-    if render:    
-        env.env.close()
-            
-    return tot_reward, reward, done, t, movie_frame
+            for _ in np.arange(history_length):
+                buffer.push(frame,0,0,False)
 
 
-def preprocess(img):
+def preprocess(img,dtype = np.float32):
 #    img_gray = np.mean(img, axis=2)
     img_gray = np.dot(img[...,:3], [0.299, 0.587, 0.114])
-    img_norm = img_gray/255.
-    img_down = imresize(img_norm,(84,84))
-    img_down = np.asarray(img_down,dtype = np.float16)
-    return img_down          
+    img_down = imresize(img_gray,(84,84))
+    img_norm = img_down/255.
+    img_norm = np.asarray(img_norm,dtype = dtype)
+    return img_norm          
 
-def optimize_model(Q,target_Q,memory,config,optimizer):
-    if len(memory) < config.BATCH_SIZE:
-        return None
-    
-    state,next_state,action,reward,done = memory.sample(config.BATCH_SIZE)
-    batch_state = state.to(device)
-    batch_action = action.to(device)
-    batch_next_state = next_state.to(device)
-    batch_reward = reward.to(device)
-    batch_done = done.to(device).byte()
 
-    current_Q = Q(batch_state).gather(1, batch_action.unsqueeze(1).long())
-
-    expected_Q = batch_reward.float()
-    expected_Q[~batch_done] += config.GAMMA * target_Q(batch_next_state[~batch_done]).max(1)[0].detach()
-
-    loss = F.mse_loss(current_Q, expected_Q.unsqueeze(1))
-    #loss = F.smooth_l1_loss(current_Q, current_Q.unsqueeze(1))
-    # Optimize the model
-    optimizer.zero_grad()
-    loss.backward()
-
-    for param in Q.parameters():
-        param.grad.data.clamp_(-1, 1)
-
-    optimizer.step()
-    return loss.detach().item(),current_Q.mean().item()
 
 def save_frames_as_gif(frames, path='./', filename='pong_animation.gif'):
 
@@ -257,7 +205,9 @@ def save_frames_as_gif(frames, path='./', filename='pong_animation.gif'):
 if __name__ == "__main__":
     class ENVIROMENT_CONFIG(object):
         ENV_NAME = 'PongDeterministic-v4'
-        RANDOM_START = 5
+        RANDOM_START = 8
+        IMG_DTYPE = np.float32
+        DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     class NN_CONFIG(ENVIROMENT_CONFIG):
         HIDDEN_SIZE = 256
         
@@ -268,11 +218,11 @@ if __name__ == "__main__":
         IMAGE_SIZE = (84,84)
         GAMMA = 0.99
         T_MAX = 5000
-        EPISODE_MAX = 1000
-        TARGET_UPDATE = 1*BASE
+        EPISODE_MAX = 5000
+        TARGET_UPDATE = 10*BASE
         EPS_0 = 1.0
         EPS_MIN = 0.1
-        EPS_LEN = 2*BUFFER_SIZE
+        EPS_LEN = 5*BUFFER_SIZE
         INITIAL_COLLECTION=50 * BASE
         REPEAT_ACTIONS = 1
         FRAME_STACK = 4
@@ -281,24 +231,56 @@ if __name__ == "__main__":
         
     config = DQN_CONFIG
     train_hist = []
-    
     env = Enviroment(config)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
     #device = torch.device('cpu')
     config.OUT_SIZE = env.env.action_space.n
-    Q = DQN(config).to(device)
+    Q = DQN(config).to(config.DEVICE)
     ####### load model ##########
     #Q.load_state_dict(torch.load('pong_Q'))
     
-    target_Q = DQN(config).to(device)
+    target_Q = DQN(config).to(config.DEVICE)
     target_Q.load_state_dict(Q.state_dict())
     target_Q.eval()
-    
     memory = ReplayMemory(config)
-    opt = optim.Adam(Q.parameters(),lr = config.LEARNING_RATE)
+    optimizer = optim.Adam(Q.parameters(),lr = config.LEARNING_RATE)
+    
+    def train_step():
+        if len(memory) < config.BATCH_SIZE:
+            return None
+        
+        batch_state,batch_next_state,batch_action,batch_reward,batch_done = memory.sample()
+        batch_state = batch_state.to(config.DEVICE)
+        batch_action = batch_action.to(config.DEVICE)
+        batch_next_state = batch_next_state.to(config.DEVICE)
+        batch_reward = batch_reward.to(config.DEVICE)
+        batch_done = batch_done.to(config.DEVICE).byte()
+    
+        current_Q = Q(batch_state).gather(1, batch_action.unsqueeze(1).long())
+    
+        expected_Q = batch_reward.float()
+        expected_Q[~batch_done] += config.GAMMA * target_Q(batch_next_state[~batch_done]).max(1)[0].detach()
+    
+        loss = F.mse_loss(current_Q, expected_Q.unsqueeze(1))
+        #loss = F.smooth_l1_loss(current_Q, current_Q.unsqueeze(1))
+        # Optimize the model
+        optimizer.zero_grad()
+        loss.backward()
+    
+        for param in Q.parameters():
+            param.grad.data.clamp_(-1, 1)
+        optimizer.step()
+        return loss.detach().item(),current_Q.mean().item()
+
+    def predict(state,eps):
+        if np.random.rand() < eps:
+            return env.env.action_space.sample()
+        q_vals = Q(state.to(config.DEVICE).unsqueeze(0)).squeeze()
+        return q_vals.argmax().item()
+    
     global_step = 0
     print("Begin initial replay memory collection.\n")
-    init_memory(env,memory,config.INITIAL_COLLECTION)
+    init_memory(env,memory,config.INITIAL_COLLECTION,config.FRAME_STACK)
     frame_buffer = History(config)
     save_list = []
     print("Begin training.")
@@ -307,12 +289,13 @@ if __name__ == "__main__":
         frame = env.new_game()
         for _ in np.arange(config.FRAME_STACK):
             frame_buffer.add(frame)
+            memory.push(frame,0,0,False)
         t_start = time.time()
         eps = max(config.EPS_MIN, config.EPS_0*(config.EPS_LEN-global_step)/config.EPS_LEN)
         for t in range(config.T_MAX):
             global_step+=1
             state = frame_buffer.get()
-            action = Q.predict(state,eps)
+            action = predict(state,eps)
             cumulative_reward = 0
             for i in np.arange(config.REPEAT_ACTIONS):    
                 frame, reward, done, info = env.step(action)
@@ -320,15 +303,12 @@ if __name__ == "__main__":
                     break
                 cumulative_reward += reward
             frame_buffer.add(frame)
-            next_state = frame_buffer.get()
             memory.push(frame, 
                         action,
                         cumulative_reward,
                         done)
             tot_reward += cumulative_reward
-            if done:
-                break  
-            loss,q_val = optimize_model(Q,target_Q,memory,config,opt)
+            loss,q_val = train_step()
             if i_episode % config.TARGET_UPDATE == 0:
                 target_Q.load_state_dict(Q.state_dict())
                 torch.save(Q.state_dict(), 'pong_Q%d'%(global_step))
@@ -336,6 +316,8 @@ if __name__ == "__main__":
                 save_list.append(('pong_Q%d'%(global_step),'pong_Q_target_%d'%(global_step)))
                 if len(save_list) > config.SAVE_LATEST:
                     [os.remove(x) for x in save_list.pop(0)]
+            if done:
+                break  
                     
         train_hist += [tot_reward]
         print("Epoch:%d Global step:%d Loss:%s Q value: %.3f Total Reward:%.2f Trail Length:%d Epsilon:%.2F Elapsed Time:%.2f Buffer size:%d"%(i_episode, global_step, loss, q_val, tot_reward, t+1, eps, time.time() - t_start, len(memory)))
@@ -348,21 +330,49 @@ if __name__ == "__main__":
 #             np.array(train_hist).reshape(-1, EPISODE_MAX).mean(axis = 1))
     plt.xlabel('# of Episode', fontsize = 20)
     plt.ylabel('Total Reward', fontsize = 20)
+
+
+    def simulate(env, horizon,config, render = False):
+        tot_reward = 0
+        frame = env.new_game()
+        frame_buffer = History(config)
+        for _ in np.arange(config.FRAME_STACK):
+            frame_buffer.add(frame)
+        movie_frame = []
+        for t in range(horizon):
+            if render:
+                #env.render()
+                env.env.render()
+                movie_frame.append(env.env.render(mode="rgb_array"))
+                time.sleep(1/24)
+                
+            state = frame_buffer.get()
+            action = predict(state,eps = 0)
+            frame, reward, done, info = env.step(action)
+            frame_buffer.add(frame)
+            tot_reward += reward
+            if done:
+                break
+                
+        if render:    
+            env.env.close()
+                
+        return tot_reward, reward, done, t, movie_frame
     
     ###
     env.random_start = 0
-    _ = simulate(env, 100, Q,config, True)
+    _ = simulate(env, 100,config, True)
     torch.save(Q.state_dict(), 'pong_Q_final')
     torch.save(target_Q.state_dict(), 'pong_Q_target_final')
 
-    Q = DQN(config).to(device)
-    target_Q = DQN(config).to(device)
+    Q = DQN(config).to(config.DEVICE)
+    target_Q = DQN(config).to(config.DEVICE)
     ####### load model ##########
     
     Q.load_state_dict(torch.load('pong_Q_final'))
     target_Q.load_state_dict(torch.load('pong_Q_target_final'))
     
     reward_tot, reward, t, done, frames = simulate(env, 500, Q,config, True)
-    save_frames_as_gif(frames[::4])
+    save_frames_as_gif(frames[::2])
     
 
